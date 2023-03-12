e5ef9f980f129a27b80b028fc4f0036b078c3de8
BlindStab <BlindStab@163.com>
2023-03-12 18:06:32 +0800

    Committer: BlindStab Reviewer: BlindStab Description: Commit Test

---
+++ src/main/java/com/demo/bean/User.java
@@ -0,0 +1,16 @@
+package com.demo.bean;
+
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String userId;
+
+    private String userName;
+
+    private String password;
+
+    private String address;
+}
